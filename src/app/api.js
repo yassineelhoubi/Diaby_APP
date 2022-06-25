ff72493e6425c30c307a37e13ffcccc3c23c0a24
Yassine ELHOUBI <elhoubiyassine@gmail.com>
2022-06-25 23:34:27 +0100

    handle login functionality

--- src/app/api.js
+++ src/app/api.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: process.env.API_URL,
+    baseUrl: "https://diaby-tracker.herokuapp.com/"
 }) ;
 
 export const api = createApi({
